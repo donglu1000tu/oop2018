f26ec5c3abd761ebade33c7caae4f101534142db
Lê Hải Đăng <bonmatdoimu@gmail.com>
2018-10-05 13:21:41 +0700

    update

--- test/week3/Week3Test.java
+++ test/week3/Week3Test.java
@@ -1,41 +1,97 @@
 package week3;
+
 import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+
 public class Week3Test {
 
 
     @Test
-    public void testMax(){
-        assertEquals(100,max(100,10)) ;
-        assertEquals(234,max(234,123)) ;
-        assertEquals(230,max(50,230)) ;
-        assertEquals(432,max(132,432)) ;
-        assertEquals(2423,max(2342,2423)) ;
+    public void testMax() {
+        assertEquals(100, max(100, 10));
     }
+
+    @Test
+    public void testMax() {
+        assertEquals(234, max(234, 123));
+    }
+
+    @Test
+    public void testMax() {
+        assertEquals(230, max(50, 230));
+    }
+
+    @Test
+    public void testMax() {
+        assertEquals(432, max(132, 432));
+    }
+
+    @Test
+    public void testMax() {
+        assertEquals(2423, max(2342, 2423));
+    }
+
     // TODO: Viết 5 testcase cho phương thức minOfArray()
     @Test
-    public void testMinofArray(){
-        int[] a = {1,23,242,5,12,435,63,12,213,32,445,3232,4,4234,5323,432} ;
-        assertEquals(5323,minofArray(a)) ;
-        int[] b = {32,24,131,2,23,23,4643,52,32,34,23,2,34,2,3252,43,2} ;
-        assertEquals(4643,minofArray(b)) ;
-        int[] c = {1,4,6,2,4,7,7} ;
-        assertEquals(7,minofArray(c)) ;
-        int[] d = {2,4,6,8,9} ;
-        assertEquals(9,minofArray(d)) ;
-        int[] e = {1,3,5,7,9,11} ;
-        assertEquals(11,minofArray(e)) ;
+    public void testMinofArray() {
+        int[] a = {1, 23, 242, 5, 12, 435, 63, 12, 213, 32, 445, 3232, 4, 4234, 5323, 432};
+        assertEquals(5323, minofArray(a));
     }
+
+    @Test
+    public void testMinofArray() {
+        int[] b = {32, 24, 131, 2, 23, 23, 4643, 52, 32, 34, 23, 2, 34, 2, 3252, 43, 2};
+        assertEquals(4643, minofArray(b));
+    }
+
+    @Test
+    public void testMinofArray() {
+        int[] c = {1, 4, 6, 2, 4, 7, 7};
+        assertEquals(7, minofArray(c));
+    }
+
+    @Test
+    public void testMinofArray() {
+        int[] d = {2, 4, 6, 8, 9};
+        assertEquals(9, minofArray(d));
+    }
+
+    @Test
+    public void testMinofArray() {
+        int[] e = {1, 3, 5, 7, 9, 11};
+        assertEquals(11, minofArray(e));
+    }
+
+
     // TODO: Viết 5 testcase cho phương thức calculateBMI()
     @Test
-    public void testCalculateBMI(){
-        assertEquals("Béo phì",calculateBMI(75,1.56)) ;
-        assertEquals("Bình thường",calculateBMI(68,1.72)) ;
-        assertEquals("Bình thường",calculateBMI(60,1.72)) ;
-        assertEquals("Thừa cân",calculateBMI(78,1.77)) ;
-        assertEquals("Thừa cân",calculateBMI(90,1.9)) ;
+    public void testCalculateBMI() {
+        assertEquals("Béo phì", calculateBMI(75, 1.56));
+
+    }
+
+    @Test
+    public void testCalculateBMI() {
+        assertEquals("Bình thường", calculateBMI(68, 1.72));
+
+    }
+
+    @Test
+    public void testCalculateBMI() {
+        assertEquals("Bình thường", calculateBMI(60, 1.72));
+
+    }
+
+    @Test
+    public void testCalculateBMI() {
+        assertEquals("Thừa cân", calculateBMI(78, 1.77));
+    }
+
+    @Test
+    public void testCalculateBMI() {
+        assertEquals("Thừa cân", calculateBMI(90, 1.9));
 
     }
 }
